d55160c7a1356fa274640937eee6b08026ae160f
LUCKYINGGG <39611265+LUCKYINGGG@users.noreply.github.com>
2025-06-01 19:50:58 -0700

    Home page and not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
-
   return (
     <>
-     <BrowserRouter>
-     <Routes>
-      <Route index element = {<Home/>}/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
 
-      <Route />
-
-     </Routes>
-     </BrowserRouter>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
