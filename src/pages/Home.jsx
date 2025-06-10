69c982d4bdc66e3960638f2cf2fb1bc0a3aaf027
LUCKYINGGG <39611265+LUCKYINGGG@users.noreply.github.com>
2025-06-09 17:00:41 -0700

    Change HeroSection to HSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
-import { HeroSection } from "../components/heroSection";
+import { HeroSection } from "../components/HSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
