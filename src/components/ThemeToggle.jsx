9087df5cc530f5f3103d9ae6999fa2e3b1661382
LUCKYINGGG <39611265+LUCKYINGGG@users.noreply.github.com>
2025-06-09 19:56:40 -0700

    enable toggle theme on mobile

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -43,7 +43,7 @@ export const ThemeToggle = () =>
     return (
         <button
             onClick={ toggleTheme }
-            className={ cn("fixed max-sm:hidden top-15 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+            className={ cn("fixed top-15 right-5 z-50 p-2 rounded-full transition-colors duration-300",
                 "focus:outlin-hidden"
             ) } >
             { isDarkMode ? (
